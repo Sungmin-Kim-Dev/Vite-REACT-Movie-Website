d35ceaf06c6b2a04622c23dee12509ba8c872355
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-06 19:12:58 +0900

    logo and footer added

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { HiHome } from "react-icons/hi";
 import { IoSearch } from "react-icons/io5";
 import { FaPlus } from "react-icons/fa";
 import { BiCameraMovie } from "react-icons/bi";
 import { FiTv } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
 import MenuItem from "./components/MenuItem";
-import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import background from "../assets/background.png";
+import logo from "../assets/logo.png";
 
 const AppLayout = () => {
   const menuItems = [
@@ -42,9 +45,9 @@ const AppLayout = () => {
   // When page changes, the mobile popup menu disappears.
   // When page changes, scrolls to the top.
   const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [pathname]);
+  // useEffect(() => {
+  //   window.scrollTo({ top: 0, behavior: "smooth" });
+  // }, [pathname]);
 
   const [keyword, setKeyword] = useState("");
   const navigate = useNavigate();
@@ -65,9 +68,9 @@ const AppLayout = () => {
           <h1>
             <Link
               to="/"
-              className="xs:mr-8 mr-2 block h-[2.375rem] w-[4.25rem] bg-slate-300 px-5 md:h-12 md:w-[5.375rem]"
+              className="mr-2 block h-[2.375rem] w-[4.25rem] xs:mr-8 md:h-12 md:w-[5.375rem]"
             >
-              LOGO
+              <img src={logo} alt="Logo" className="size-full" />
             </Link>
           </h1>
           <nav className="mr-auto">
@@ -100,9 +103,40 @@ const AppLayout = () => {
           </form>
         </div>
       </header>
-      <main className='pb-10'>
+      <main className="pb-14">
         <Outlet />
       </main>
+      <footer className="global-px bg-black pb-8">
+        <Link to="/" className="mx-auto block w-[4.25rem] py-4 md:w-[5.375rem]">
+          <img src={logo} alt="Logo" className="size-full" />
+        </Link>
+        <div className="f-text-box">
+          <p className="text-lg">
+            This is a demo website for my REACT Project.
+          </p>
+          <p className="text-lg">This website is created with:</p>
+          <p className="pl-2 text-lg">
+            Vite, REACT, Tailwind CSS, shadcn/UI, REACT Router, TanStack Query,
+            Axios, TMDB API, react-i18next
+          </p>
+          <div className="link-box flex flex-wrap items-center gap-x-10 gap-y-4 pt-4">
+            <a
+              href="https://github.com/Sungmin-Kim-Dev/Vite-REACT-Movie-Website"
+              className="flex items-center gap-2 hover:underline"
+            >
+              <FaGithub className="size-6" />
+              <span className="text-lg">Github Repository</span>
+            </a>
+            <a
+              href="mailto: dreamerk24dev@gmail.com"
+              className="flex items-center gap-2 text-lg hover:underline"
+            >
+              <MdOutlineEmail className='size-6'/>
+              dreamerk24dev@gmail.com
+            </a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
